b553dc9101752bc6e2b2ac9e3a12b23c5575952d
Rob Bednark <rbednark@gmail.com>
2014-10-13 00:16:17 -0700

    Auto commit from vici

--- date_funcs.js
+++ date_funcs.js
@@ -1,5 +1,34 @@
 // Note: jquery is required, e.g., http://code.jquery.com/jquery-1.8.3.js
 
+function date_plus_business_days(date_start, num_days_business, holidays) {
+    "use strict";
+    var ONE_DAY, current_date, num_days_so_far;
+    ONE_DAY = 24 * 60 * 60 * 1000;
+    current_date = date_start + ONE_DAY;
+    num_days_so_far = 0;
+    while (num_days_so_far < num_days_business) {
+        if is_business_day(current_date, holidays) {
+            ++num_days_so_far;
+        }
+        if (num_days_so_far < num_days_business) {
+            current_date += ONE_DAY;
+        }
+    }
+    return current_date;
+}
+
+function is_business_day(date, holidays) {
+    "use strict";
+    if (date.getDay() === 0 || date.getDay() === 6) {
+        // ASSERT: day is Sunday or Saturday
+        return false;
+    }
+    if (is_holiday(date, holidays)) {
+        return false;
+    }
+    return true;
+}
+
 function is_holiday(date, holidays, timezone) {
     // Note that {date} is a Date object, but {holidays} is an array of strings,
     // like ["12/25/2014"]
@@ -18,16 +47,4 @@ function is_holiday(date, holidays, timezone) {
         }
     }
     return false;
-}
-
-function is_business_day(date, holidays) {
-    "use strict";
-    if (date.getDay() === 0 || date.getDay() === 6) {
-        // ASSERT: day is Sunday or Saturday
-        return false;
-    }
-    if (is_holiday(date, holidays)) {
-        return false;
-    }
-    return true;
 }
